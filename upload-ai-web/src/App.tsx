a30f304b5d4b06ca5229d5887d5858de8165997e
DarlanZero <darliankeira229@gmail.com>
2023-09-18 00:46:41 -0300

    final project ready!

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -7,8 +7,31 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 import { Slider } from "./components/ui/slider";
 
 import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
+import { useCompletion } from 'ai/react'
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -33,11 +56,14 @@ export function App() {
             <Textarea
               className="resize-none p-5 leading-relaxed"
               placeholder="inclua o prompt para a IA..." 
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className="resize-none p-5 leading-relaxed"
               placeholder="Resultado gerado pela IA!..." 
               readOnly 
+              value={completion}
             />
           </div>
 
@@ -47,23 +73,15 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
 
             <div className="space-y-2 ">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um Prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className="space-y-2 ">
@@ -91,6 +109,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
 
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
@@ -100,7 +120,7 @@ export function App() {
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2" />
             </Button>
--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -8,8 +8,23 @@ import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 import { api } from "@/lib/axios";
 
-export function VideoInputForm() {
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!',
+
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -74,7 +89,7 @@ export function VideoInputForm() {
       return
     }
 
-    // converter o vídeo em audio
+    setStatus('converting')
     
     const audioFile = await convertVideoToAudio(videoFile)
 
@@ -82,13 +97,21 @@ export function VideoInputForm() {
 
     data.append('file', audioFile)
 
+    setStatus('uploading')
+
     const response = await api.post('/videos', data)
 
     const videoId = response.data.video.id
 
+    setStatus('generating')
+
     await api.post(`/videos/${videoId}/transcription`, {
       prompt,
     })
+
+    setStatus('success')
+
+    props.onVideoUploaded(videoId)
 
   }
 
@@ -125,6 +148,7 @@ export function VideoInputForm() {
             <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
             <Textarea 
               ref={promptInputRef}
+              disabled={status != 'waiting'}
               id="transcription_prompt" 
               className="h-20 leading-relaxed resize-none" 
               placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
@@ -132,9 +156,18 @@ export function VideoInputForm() {
             />
           </div>
 
-          <Button type="submit" className="w-full">
-            Carregar vídeo
-            <Upload className="w-4 h-4 ml-2" />
+          <Button 
+          data-success={status == 'success'}
+          disabled={status != 'waiting'} 
+          type="submit" 
+          className="w-full data-[success=true]:bg-emerald-600"
+          >
+            {status == 'waiting' ? (
+              <>
+                Carregar Video
+                <Upload className="w-4 h-4 ml-2" />
+              </>
+            ): statusMessages[status] }
           </Button>
         </form>
   )
